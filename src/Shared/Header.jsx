2a1ce0243d108373190086682fc9132d30606b7e
shihab0 <ataharshihab1@gmail.com>
2022-10-27 15:13:40 +0600

    toggle button added

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -6,17 +6,27 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import userPhoto from '../assest/img/user-512.webp';
 import { FcReading } from "react-icons/fc";
+import { BsMoonFill, BsSun } from "react-icons/bs";
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthProvider/AuthProvider';
 import Image from 'react-bootstrap/Image';
+import { useState } from 'react';
+import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
 
 const Header = () => {
+    const [nightMode, setNightMode] = useState(false);
 
     const { logOut } = useContext(AuthContext);
+
+    const modeChange = () => {
+        setNightMode(!nightMode);
+        }
+    }
+
     const handleLogOut = () => {
         logOut()
-        .then()
-        .catch(err => console.error(err))
+            .then()
+            .catch(err => console.error(err))
     }
 
     const { user } = useContext(AuthContext);
@@ -30,7 +40,10 @@ const Header = () => {
                     <Nav className="me-auto">
                         <Nav.Link><Link className='text-decoration-none' to='/home'>Home</Link></Nav.Link>
                         <Nav.Link><Link className='text-decoration-none' to='/allCourse'>Course</Link></Nav.Link>
-                        <Nav.Link><Link className='text-decoration-none' to='/blogs'>Blogs</Link></Nav.Link> 
+                        <Nav.Link><Link className='text-decoration-none' to='/blogs'>Blogs</Link></Nav.Link>
+                        {
+                            nightMode ? <Nav.Link><Link onClick={modeChange} className='text-decoration-none text-white'><BsSun></BsSun> </Link></Nav.Link> : <Nav.Link><Link onClick={modeChange} className='text-decoration-none text-white'><BsMoonFill /> </Link></Nav.Link>
+                        }
                     </Nav>
                     <Nav>
                         <Nav.Link>
@@ -41,7 +54,7 @@ const Header = () => {
                         <Nav.Link>
                             {
                                 user?.uid ? <><Link onClick={handleLogOut} className='text-decoration-none' to='/login'>Logout</Link></>
-                                : <><Link className='text-decoration-none' to='/login'>Login</Link></>
+                                    : <><Link className='text-decoration-none' to='/login'>Login</Link></>
                             }
                         </Nav.Link>
                         <Nav.Link>
